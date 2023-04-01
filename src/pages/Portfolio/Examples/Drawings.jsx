70b1a66dd0d0b5638347350df3d5b7fde36dc098
Natalia Carvalho <ssspinosaure@gmail.com>
2023-04-01 15:17:27 -0300

    added a drawing section

--- src/pages/Portfolio/Examples/Drawings.jsx
+++ src/pages/Portfolio/Examples/Drawings.jsx
@@ -1,22 +1,14 @@
 import React from "react";
+import ScrollToTop from "react-scroll-to-top";
 import "../Examples/css/drawing.css";
 
 function Drawings() {
   return (
     <>
+      <div style={{ marginTop: "50px" }} />
+      <ScrollToTop smooth />
       <article className="drawing">
         <section className="grid grid-cols-2 lg:grid-cols-3">
-          <div class="container">
-            <img
-              className="container__image"
-              src="https://imgur.com/2c72AjK.jpg"
-              alt="drawing"
-            />
-            <div class="container__overlay">
-              <div class="container__text">test</div>
-            </div>
-          </div>
-
           <div className="py-5 px-10">
             <img
               className="rounded-lg"
